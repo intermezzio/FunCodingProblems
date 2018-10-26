82792891ef7fad4f15a2d05333974d35da5b22d6
Andrew Mascillaro <Andrew.Mascillaro@icims.com>
2018-10-26 16:59:53 -0400

    Work on grid problem

--- gridsolver.py
+++ gridsolver.py
@@ -7,4 +7,17 @@ x 1 4 5 9
 Starting from x and only going either right or down, find the path with the highest sum of numbers
 (given any grid of size m*n)
 """
+import np
+test = [
+	[0, 3, 4],
+	[2, 5, 6],
+	[1, 3, 0]
+]
 
+
+def gridsolve(grid):
+	subsections = np.full(grid.shape, -1)
+	def recursiveSolve(i,j,grid):
+		if subsections[i,j] != -1:
+			return subsections[i,j]
+		elif subsections[i,j]
