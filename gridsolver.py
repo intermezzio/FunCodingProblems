dc9e2edaeeacb91b6b84aae9b101d3b89f7c553f
Andrew Mascillaro <anmascillaro@ctemc.org>
2018-10-26 16:26:14 -0400

    Create gridsolver.py

---
+++ gridsolver.py
@@ -0,0 +1,10 @@
+"""
+Solve the following problem:
+x 1 4 5 9
+2 3 9 8 3
+1 3 2 4 2
+2 7 7 6 y
+Starting from x and only going either right or down, find the path with the highest sum of numbers
+(given any grid of size m*n)
+"""
+
